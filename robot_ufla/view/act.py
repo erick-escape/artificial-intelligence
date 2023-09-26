d808fa7dea03a7d82c0d1470e7baa0374aaee35a
Érick de Castro <erick.silva@estudante.ufla.br>
2023-09-25 23:32:53 -0300

    Addig pyttsx3

--- robot_ufla/view/act.py
+++ robot_ufla/view/act.py
@@ -1,18 +1,19 @@
-# import pyttsx3
+import pyttsx3
 import time
 
-from elevenlabs import generate, play, set_api_key
+# from elevenlabs import generate, play, set_api_key
 
 # set_api_key("")
 
 
 def act(str):
-    # engine = pyttsx3.init()
-    # engine.say(str)
-    # time.sleep(3)
-    # engine.runAndWait()
-    # time.sleep(3)
-    audio = generate(text=str, api_key="",
-                     voice="Daniel", model="eleven_multilingual_v2")
-    play(audio)
+    engine = pyttsx3.init()
+    engine.setProperty('voice', "brazil")
+    engine.say(str)
     time.sleep(3)
+    engine.runAndWait()
+    time.sleep(3)
+    # audio = generate(text=str, api_key="",
+    #                  voice="Daniel", model="eleven_multilingual_v2")
+    # play(audio)
+    # time.sleep(3)
