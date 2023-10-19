8bddbc6da977570da803b6509c0a78c7e0f2ea8c
Érick de Castro <erick.silva@estudante.ufla.br>
2023-10-18 21:16:13 -0300

    adding while loop with the whole proccess

--- geneticAlgorithm/main.py
+++ geneticAlgorithm/main.py
@@ -1,19 +1,42 @@
+"""
+Authors: Érick de Castro Silva, Luiz Fernando Ferreira Santos
+
+Função: x^2 - 3x + 4, encontre o valor máximo.
+
+- Assumir que x [-10, +10].
+- Codificar X como vetor binário.
+- Criar uma população inicial com 4  indivíduos.
+- Aplicar Mutação com taxa de 1%.
+- Aplicar Crossover com taxa de 70%.
+- Usar seleção por torneio.
+- Usar 5  gerações.
+
+"""
+
 import geneticAlgorithm as GA
 
-genes = 4
-chromosomes = 4
 generations = 5
-firstChromosomes = [
-    ['+ 0001', 0],
-    ['+ 0010', 0],
-    ['- 0001', 0],
-    ['- 0010', 0],
-]
+# calculate the fitness of the first generation
+chromosomes = GA.calculateFitnessOfAllChromosomes(
+    GA.getFirstGeneration())
+print(chromosomes)
 
+generationNumber = 1
+while (generationNumber <= generations):
+    # by tournament selection we choose two chromosomes
+    firstSelected = GA.selectionByTournment(
+        chromosomes)
+    secondSelected = GA.selectionByTournment(
+        chromosomes)
+    
+    # crossing them over and calculating their fitness
+    crossoveredChromosomes = GA.crossover(firstSelected, secondSelected)
+    crossoveredChromosomes = GA.calculateFitnessOfAllChromosomes(
+        crossoveredChromosomes)
 
-while (generations):
-    chromosomesFitnesses = GA.calculateFitnessOfAllChromosomes(
-        firstChromosomes)
-    selectedChromosomes = GA.selectionByTournment(
-        chromosomesFitnesses)
-    generations -= 1
+    # appling mutation to the chromosomes
+    for chromosome in crossoveredChromosomes:
+        chromosomes.append(GA.mutation(chromosome))
+    chromosomes = GA.calculateFitnessOfAllChromosomes(chromosomes)
+    GA.printGeneration(chromosomes, generationNumber)
+    generationNumber += 1
