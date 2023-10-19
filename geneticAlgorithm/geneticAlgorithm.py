b3cd4249f77e25af0dd25aeb463e321d795126cd
Érick de Castro <erick.silva@estudante.ufla.br>
2023-10-18 21:16:41 -0300

    adding two complements functions

--- geneticAlgorithm/geneticAlgorithm.py
+++ geneticAlgorithm/geneticAlgorithm.py
@@ -1,87 +1,91 @@
-"""
-Authors: Érick de Castro Silva, Luiz Fernando Ferreira Santos
-
-Função: x^2 - 3x + 4, encontre o valor máximo.
-
-- Assumir que x [-10, +10].
-- Codificar X como vetor binário.
-- Criar uma população inicial com 4  indivíduos.
-- Aplicar Mutação com taxa de 1%.
-- Aplicar Crossover com taxa de 70%.
-- Usar seleção por torneio.
-- Usar 5  gerações.
-
-"""
-
 import random
 
-crossoverTax = 0.7
-mutation = 0.1
+genes = 4
+chromosomes = 4
+crossoverRate = 70
+mutationRate = 1
+crossoverPoint = 2
 
+def decimalToTwosComplement(decimal, bits=5):
+    if decimal >= 0:
+        binary = bin(decimal)[2:].zfill(bits)
+    else:
+        abs_decimal = abs(decimal)
+        oneComplement = bin(abs_decimal - 1)[2:]
+        twosComplement = ''.join('1' if bit == '0' else '0' for bit in oneComplement.zfill(bits))
+        binary = twosComplement
+
+    return binary
+
+def twosComplementToDecimal(binary):
+    if binary[0] == '1':
+        oneComplement = ''.join('1' if bit == '0' else '0' for bit in binary)
+        twosComplement = bin(int(oneComplement, 2) + 1)[2:]
+        decimal = -int(twosComplement, 2)
+        return decimal
+    else:
+        decimal = int(binary, 2)
+        return decimal
+
+def getFirstGeneration():
+    generation = []
+    for i in range(0, genes, 1):
+        generation.append([decimalToTwosComplement(random.randint(-10, 10)), 0])
+    return generation
 
 def fitness(x):
     return x*x - 3 * x + 4
 
-
 def calculateFitnessOfAllChromosomes(chromosomes):
-    i = 0
-    for chromoso in chromosomes:
-        # separate sign from value
-        splitChromoso = chromoso[0].split()
-        chromosoSign = splitChromoso[0]
-        chromosoValue = splitChromoso[1]
+    for chromosome in chromosomes:
+        # getting decimal to calculate fitness
+        decimal = twosComplementToDecimal(chromosome[0])
 
         # chromoso at 1 receives its fitness
-        chromoso[1] = fitness(int(chromosoSign + chromosoValue, 2))
-        chromosomes[i] = chromoso
-        i += 1
+        chromosome[1] = fitness(decimal)
     return chromosomes
 
-
 def selectionByTournment(chromosomes):
-    twoSelected = [[], []]
-    max = 0
-    secondMax = 0
-    if (chromosomes[0][1] >= chromosomes[1][1]):
-        max = chromosomes[0]
-        secondMax = chromosomes[1]
+    randomChromosomeA = random.randint(0, len(chromosomes) - 1)
+    randomChromosomeB = random.randint(0, len(chromosomes) - 1)
+    if (chromosomes[randomChromosomeA][1] > chromosomes[randomChromosomeB][1]):
+        winnerChromosome = chromosomes[randomChromosomeA]
+        del chromosomes[randomChromosomeA]
     else:
-        max = chromosomes[1]
-        secondMax = chromosomes[0]
-    for i in range(2, len(chromosomes), 1):
-        if (chromosomes[i][1] > max[1]):
-            secondMax = max
-            max = chromosomes[i]
-        elif (chromosomes[i][1] > secondMax[1]):
-            secondMax = chromosomes[i]
-    twoSelected[0] = max
-    twoSelected[1] = secondMax
-    return twoSelected
-
+        winnerChromosome = chromosomes[randomChromosomeB]
+        del chromosomes[randomChromosomeB]
+    return winnerChromosome
 
 def crossover(chromosomeA, chromosomeB):
-    # getting sign and value from chromosomes
-    signAndValueArrayA = chromosomeA[0].split()
-    signAndValueArrayB = chromosomeB[0].split()
+    randomInteger = random.randint(1, 100)
+    if (randomInteger <= crossoverRate):
+        # setting crossoverPoint and getting chromosomes' "tails"
+        chromosomeATail = chromosomeA[0][crossoverPoint:]
+        chromosomeBTail = chromosomeB[0][crossoverPoint:]
 
-    # setting crossOverPoint and getting chromosomes' "tails"
-    crossOverPoint = int(len(signAndValueArrayA[1]) * crossoverTax)
-    chromosomeATail = signAndValueArrayA[1][crossOverPoint:]
-    chromosomeBTail = signAndValueArrayB[1][crossOverPoint:]
-
-    # crossing over
-    signAndValueArrayA[1] = signAndValueArrayA[:crossOverPoint] + \
-        chromosomeBTail
-    signAndValueArrayB[1] = signAndValueArrayB[:crossOverPoint] + \
-        chromosomeATail
-
-    chromosomeA[0] = signAndValueArrayA[0] + ' ' + signAndValueArrayA[1]
-    chromosomeB[0] = signAndValueArrayA[0] + ' ' + signAndValueArrayA[1]
-    return chromosomeA, chromosomeB
-
+        # crossing over
+        if (-10 <= twosComplementToDecimal(chromosomeA[0][:crossoverPoint] + chromosomeBTail) <= 10):
+            chromosomeA[0] = chromosomeA[0][:crossoverPoint] + chromosomeBTail
+        elif (-10 <= twosComplementToDecimal(chromosomeB[0][:crossoverPoint] + chromosomeATail) <= 10):
+            chromosomeB[0] = chromosomeB[0][:crossoverPoint] + chromosomeATail
+    return [chromosomeA, chromosomeB]
 
 def mutation(chromosome):
-    genesQuantity = len(chromosome)
-    randomGene = random.randint(0, genesQuantity - 1)
-    chromosome[randomGene] = not chromosome[randomGene]
+    # getting sign and value from chromosomes
+    newValue = ''
+    for bit in chromosome[0]:
+        randomInteger = random.randint(1, 100)
+        if (randomInteger <= mutationRate):
+            newValue += str(int(not bit))
+        else:
+            newValue += str(bit)
+
+    if (-10 <= twosComplementToDecimal(newValue) <= 10):
+        chromosome[0] = newValue
     return chromosome
+
+def printGeneration(chromosomes, generationNumber):
+    print("\nGeneration: ", generationNumber)
+    print("\nChromosomes:\n\n")
+    for chromosome in chromosomes:
+        print("Chromosome: ", chromosome[0], " - Fitness: ", chromosome[1])
