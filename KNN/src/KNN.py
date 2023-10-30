be94dfa23e840bd243c2c1b33bda8b2d1998630b
Érick de Castro <erick.silva@estudante.ufla.br>
2023-10-30 20:52:28 -0300

    adding prototypes tests

--- KNN/src/KNN.py
+++ KNN/src/KNN.py
@@ -6,88 +6,82 @@ import random
 
 
 class KNN:
+    def __init__(self, neighbors=1, method="mean"):
+        self._neighbors = neighbors if neighbors >= 1 else 1
+        self._dict_methods = dict({
+            "mean": partial(self._mean),
+            "median": partial(self._median),
+            "mode": partial(self._mode),
+            "harmonic_mean": partial(self._harmonic_mean),
+            "mean_min_max": partial(self._mean_min_max),
+            "random": partial(self._random)
+        })
+        self._method = method if method in self._dict_methods.keys() else "mean"
+        self._prototypes = dict({})
+        self._dict_individuals = dict({})
 
-    def __init__(self, vizinhos=1, metodo="media"):
-        # Armazena a quantidade de "prototipos"
-        self._vizinhos = vizinhos if vizinhos >= 1 else 1
-        self._dict_metodos = dict({
-            "media": partial(self._media),
-            "mediana": partial(self._mediana),
-            "moda": partial(self._moda),
-            "media_harmonica": partial(self._media_harmonica),
-            "media_min_max": partial(self._media_min_max),
-            "aleatorio": partial(self._aleatorio)
-        })  # Armazena os tipos e os metodos para achar o "protipo"
-        # Armazerna qual metodo para achar prototipo usar
-        self._metodo = metodo if metodo in self._dict_metodos.keys() else "media"
-        self._prototipos = dict({})  # Armazena os prototipos
-        self._dict_individuos = dict({})  # Armazena os individuos
+    def _euclidean_distance(self, A, B):
+        return spatial.distance.euclidean(A, B)
 
-    # Calcula distância Euclidiana
-    def _distancia_euclidiana(self, individuo_A, individuo_B):
-        return spatial.distance.euclidean(individuo_A, individuo_B)
+    def _mean(self, matrix):
+        return np.mean(matrix, axis=0)
 
-    def _media(self, matriz):  # Calcula a média
-        return np.mean(matriz, axis=0)
+    def _median(self, matrix):
+        return np.median(matrix, axis=0)
 
-    def _mediana(self, matriz):  # Calcula a mediana
-        return np.median(matriz, axis=0)
+    def _mode(self, matrix):
+        return stats.mode(matrix, axis=0)
 
-    def _moda(self, matriz):  # Calcula a moda
-        return stats.mode(matriz, axis=0)
+    def _harmonic_mean(self, matrix):
+        return stats.hmean(matrix, axis=0)
 
-    def _media_harmonica(self, matriz):  # Calcula a média harmonica
-        return stats.hmean(matriz, axis=0)
+    def _mean_min_max(self, matrix):
+        matrix_min = np.min(matrix, axis=0)
+        matrix_max = np.max(matrix, axis=0)
+        matrix_min_max = np.array([matrix_min, matrix_max])
+        return np.mean(matrix_min_max, axis=0)
 
-    # Calcula a média usando os valores min e max de cada atributo
-    def _media_min_max(self, matriz):
-        matriz_min = np.min(matriz, axis=0)
-        matriz_max = np.max(matriz, axis=0)
-        matriz_min_max = np.array([matriz_min, matriz_max])
-        return np.mean(matriz_min_max, axis=0)
+    def _random(self, matrix):  # Makes a random choice to get the prototype
+        return random.choice(matrix)
 
-    def _aleatorio(self, matriz):  # Escolhe alguem para ser o prototipo
-        return random.choice(matriz)
+    def training(self, X_training, Y_training):
+        attributes = X_training.columns
+        Y_training.rename("results", inplace=True)
+        data = pd.concat([X_training, Y_training], axis=1)
+        # Makes a dictionary, in which the key will be the set and the value a list with this sets individuals
+        self._dict_individuals = dict({group[0]: np.array(
+            group[1][attributes].values.tolist()) for group in data.groupby(by="results")})
+        for group in self._dict_individuals.keys():  # For to create the prototypes
+            fictitious_prototype = self._dict_methods[self._method](
+                self._dict_individuals[group])
+            individuals = self._dict_individuals[group].copy()
+            for individual in individuals:
+                # Calculates the euclidean distance between fictitious prototype and all the sets individuals
+                list(individual).append(self._euclidean_distance(
+                    fictitious_prototype, individual))
+            self._prototypes.update(dict(
+                {group: individuals[individuals[:, -1].argsort()][:self._neighbors]}))
 
-    def treinamento(self, X_treino, Y_treino):  # Realiza o treinamento
-        lista_atributos = X_treino.columns  # Pega as colunas de atributos
-        Y_treino.rename("Resultados", inplace=True)  # Muda o nome da coluna
-        dados = pd.concat([X_treino, Y_treino], axis=1)  # Junta os dados
-        # Cria um dicionário, no qual a chave será, o conjunto, e o valor uma lista com os individuos desse conjunto
-        self._dict_individuos = dict({grupo[0]: np.array(
-            grupo[1][lista_atributos].values.tolist()) for grupo in dados.groupby(by="Resultados")})
-        for grupo in self._dict_individuos.keys():  # For para criar os prototipos
-            # Cria um prototipo ficticio, usando o metodo escolhido
-            prototipo_ficticio = self._dict_metodos[self._metodo](
-                self._dict_individuos[grupo])
-            lista_individuos = self._dict_individuos[grupo].copy()
-            for individuo in lista_individuos:
-                # Calcula a distância euclidiana, entre prototipo ficticio, para todos os individuos do conjunto
-                list(individuo).append(self._distancia_euclidiana(
-                    prototipo_ficticio, individuo))
-            self._prototipos.update(dict(
-                {grupo: lista_individuos[lista_individuos[:, -1].argsort()][:self._vizinhos]}))  # Salva os protipos
-
-    def predicao(self, X_teste):  # Realiza a predição
-        resultados = []
-        for individuo in X_teste.values.tolist():  # Percorre os individuos que devem ser preditos
-            resultado_preliminar = dict({})
-            for grupo in self._prototipos.keys():
-                resultado_preliminar.update(dict({grupo: []}))
-                for prototipo in self._prototipos[grupo]:
-                    # Calcula a distância euclidiana entre os prototipos para os individuos
-                    resultado_preliminar[grupo].append(
-                        self._distancia_euclidiana(prototipo, individuo))
-                # Pego a menor distância encontrada para o conjunto
-                resultado_preliminar[grupo] = min(resultado_preliminar[grupo])
-            # Pego qual conjunto foi menor
-            resultados.append(
-                min(resultado_preliminar, key=resultado_preliminar.get))
-        return resultados
+    def prediction(self, X_test):
+        results = []
+        for individual in X_test.values.tolist():  # Individuals to be predicted
+            partial_result = dict({})
+            for group in self._prototypes.keys():
+                partial_result.update(dict({group: []}))
+                for prototype in self._prototypes[group]:
+                    # Calculates the euclidean distance between prototypes and individuals
+                    partial_result[group].append(
+                        self._euclidean_distance(prototype, individual))
+                # Gets the smallest distance encountered for the set
+                partial_result[group] = min(partial_result[group])
+            # Gets the smallest set
+            results.append(
+                min(partial_result, key=partial_result.get))
+        return results
 
     @property
-    def prototipos(self):  # Imprimi os prototipos de cada conjunto
-        for grupo in self._prototipos.keys():
-            print("Os prototipos do grupo " + str(grupo) + " são:")
-            for individuo in self._prototipos[grupo]:
-                print(" - " + str(individuo))
+    def prototypes(self):  # Print the set prototypes
+        for set in self._prototypes.keys():
+            print("Sets prototypes " + str(set) + ":")
+            for individual in self._prototypes[set]:
+                print(" - " + str(individual))
--- KNN/src/main.py
+++ KNN/src/main.py
@@ -1,8 +1,28 @@
+# Authors: Érick de Castro Silva e Luís Fernando Ferreira Santos
+
 from KNN import KNN
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-knn = KNN(vizinhos=1)
+knn = KNN(neighbors=1)
 dataset = pd.read_csv("../data/iris.csv")
-print(dataset)
+X = dataset.drop(columns=["Id", "Species"])
+Y = dataset["Species"]
+
+X_training, X_test, Y_training, Y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=7)
+
+# TESTS
+
+method = "median"
+print("METHOD: ", method)
+for i in range(1, 7, 2):
+    print("NEIGHBORS: ", i, "\n")
+    knn = KNN(neighbors=i, method=method)
+    knn.training(X_training, Y_training)
+    print("\nPROTOTYPES:\n")
+    knn.prototypes
+    result = knn.prediction(X_test)
+    print("\nMETRICS:\n")
+    print(metrics.classification_report(Y_test, result, digits=3), "\n")
